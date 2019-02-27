051db25753f542ddb62edea05d3ff63043e7b57b
berkgokden <berkgokden@gmail.com>
2019-02-27 11:48:17 +0100

    Remove panic in the case of config not found due to version command

--- cmd/root.go
+++ cmd/root.go
@@ -116,8 +116,8 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err != nil {
-		panic(err)
-		//fmt.Println("Using config file:", viper.ConfigFileUsed())
+		// For some commands config is not needed so don't panic
+		fmt.Println("Configuration file not found:", viper.ConfigFileUsed())
 	}
 	// unmarshal config
 	c := viper.AllSettings()
