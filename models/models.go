b24a277ffc7e65b4d3ec7112da0bc9fbee4d2a57
Alessandro Valcepina <alessandro@magnetic.io>
2019-03-05 13:35:41 +0100

    Added missing vault fields and creating linux release

--- models/models.go
+++ models/models.go
@@ -177,8 +177,20 @@ type SqlConfiguration struct {
 }
 
 type VaultKeyValueStoreConfiguration struct {
-	Url   string `yaml:"url,omitempty" json:"url,omitempty"`
-	Token string `yaml:"token,omitempty" json:"token,omitempty"`
+	Url               string `yaml:"url,omitempty" json:"url,omitempty"`
+	Token             string `yaml:"token,omitempty" json:"token,omitempty"`
+	KvMode            string `yaml:"kv-mode,omitempty" json:"kv-mode,omitempty"`
+	FallbackKvVersion int    `yaml:"fallback-kv-versione,omitempty" json:"fallback-kv-version,omitempty"`
+	ServerTlsCert     string `yaml:"server-tls-cert,omitempty" json:"server-tls-cert,omitempty"`
+	ClientTlsLey      string `yaml:"client-tls-key,omitempty" json:"client-tls-key,omitempty"`
+	ClientTlsCert     string `yaml:"client-tls-cert,omitempty" json:"client-tls-cert,omitempty"`
+	/*
+
+
+		private lazy val serverTlsCert = configString("server-tls-cert")
+		private lazy val clientTlsKey = configString("client-tls-key")
+		private lazy val clientTlsCert = configString("client-tls-cert")
+	*/
 }
 
 type KeyValueStoreConfiguration struct {
