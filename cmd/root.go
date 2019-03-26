bd4d464cb7701e3aed527bfd48a6b2270cf6b92c
Alessandro Valcepina <alessandro@magnetic.io>
2019-03-26 10:09:41 +0100

    Increasing version

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ import (
 const AppName string = "forklift"
 
 // version should be in format d.d.d where d is a decimal number
-const Version string = "0.1.10"
+const Version string = "0.1.11"
 
 /*
 Application name can change over time so it is made parameteric
