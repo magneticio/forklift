f587c8d7e6b0a4bad1e4fecf873c97d2a4f23066
berkgokden <berkgokden@gmail.com>
2019-04-15 15:13:30 +0200

    Add explicit call for dependency

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 go get -v ./...
+set -x
 go get github.com/stretchr/testify
 go get github.com/DATA-DOG/go-sqlmock
 go get github.com/hashicorp/vault
+go get github.com/hashicorp/vault/helper/strutil
