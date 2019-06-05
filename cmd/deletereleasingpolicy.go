5b64f359c5e9b42d0618dd8f90ac9feaea6f2102
berkgokden <berkgokden@gmail.com>
2019-06-05 11:47:53 +0200

    Added delete realing policy VE-757

---
+++ cmd/deletereleasingpolicy.go
@@ -0,0 +1,80 @@
+// Copyright © 2019 Developer <developer@vamp.io>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/magneticio/forklift/core"
+	"github.com/magneticio/forklift/logging"
+	"github.com/magneticio/forklift/util"
+	"github.com/spf13/cobra"
+)
+
+var deleteReleasingpolicyCmd = &cobra.Command{
+	Use:   "releasingpolicy",
+	Short: "Delete a releasingpolicy",
+	Long: AddAppName(`Delete a new releasingpolicy
+    Example:
+    $AppName delete releasingpolicy name --organization org --environment env --file ./releasingpolicydefinition.json -i json`),
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("Not Enough Arguments, Releasing Policy name needed.")
+		}
+		name := args[0]
+
+		if !util.ValidateName(name) {
+			return errors.New("Releasing Policy name should be only lowercase alphanumerics")
+		}
+
+		logging.Info("Deleteing new releasingpolicy to environment %v in organization %v\n", organization, environment)
+		namespacedEnvironment := Config.Namespace + "-" + organization + "-" + environment
+		namespacedOrganization := Config.Namespace + "-" + organization
+
+		core, coreError := core.NewCore(Config)
+		if coreError != nil {
+			return coreError
+		}
+
+		createreleasingpolicyError := core.DeleteReleasingPolicy(namespacedOrganization, namespacedEnvironment, name)
+		if createreleasingpolicyError != nil {
+			return createreleasingpolicyError
+		}
+
+		fmt.Printf("Releasing Policy is deleted\n")
+
+		return nil
+	},
+}
+
+func init() {
+	deleteCmd.AddCommand(deleteReleasingpolicyCmd)
+
+	deleteReleasingpolicyCmd.Flags().StringVarP(&organization, "organization", "", "", "Organization of the workflow")
+	deleteReleasingpolicyCmd.MarkFlagRequired("organization")
+
+	deleteReleasingpolicyCmd.Flags().StringVarP(&environment, "environment", "", "", "Environment of the workflow")
+	deleteReleasingpolicyCmd.MarkFlagRequired("environment")
+
+}
