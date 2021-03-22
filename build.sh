3a5dcf3d447922b1163578c33b7862f252ecd9a1
sandro <sandro.jaya@gmail.com>
2021-03-22 15:44:55 +0100

    added arm64 binary support

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ GOIMAGE="dockercore/golang-cross:1.12.3"
 
 if [ "$1" = "local" ]; then
   for GOOS in darwin linux windows; do
-    for GOARCH in 386 amd64; do
+    for GOARCH in 386 amd64 arm64; do
       echo "Building $GOOS-$GOARCH"
       export GOOS=$GOOS
       export GOARCH=$GOARCH
@@ -25,7 +25,7 @@ if [ "$1" = "local" ]; then
 else
   docker run --rm -it -v $(pwd):/src -w /src $GOIMAGE sh -c '
   for GOOS in darwin linux windows; do
-    for GOARCH in 386 amd64; do
+    for GOARCH in 386 amd64 arm64; do
       echo "Building $GOOS-$GOARCH"
       export GOOS=$GOOS
       export GOARCH=$GOARCH
